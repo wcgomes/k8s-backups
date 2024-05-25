0d4ee82d26c5774414d3e0ed48ca51f58589c7a3
Wyller Gomes <wyller.gomes@plantsuite.com>
2024-05-24 22:05:52 -0300

    Coleta de telemetria desativada por padrão.

--- mongodb-sync/mongosync.sh
+++ mongodb-sync/mongosync.sh
@@ -3,11 +3,6 @@
 set -e
 
 SCRIPT_NAME=mongodb-cluster-to-cluster-sync
-DISABLE_TELEMETRY=""
-
-if [ -n "$DISABLE_TELEMETRY" ]; then
-	DISABLE_TELEMETRY="--disableTelemetry"
-fi
 
 # Print tool version
 mongosync --version
@@ -16,10 +11,11 @@ echo ""
 echo "[$SCRIPT_NAME] Starting..."
 
 # Run mongosync
-mongosync $DISABLE_TELEMETRY \
+mongosync \
     --cluster0="$CLUSTER0_URI" \
     --cluster1="$CLUSTER1_URI" \
     --loadLevel="$LOAD_LEVEL" \
-    --verbosity="$VERBOSITY"
+    --verbosity="$VERBOSITY" \
+    --disableTelemetry 
 
 echo "[$SCRIPT_NAME] Stopped."
